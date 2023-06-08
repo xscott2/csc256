ae9d0284fb2cf5d76ca7f76ba4e697ec4f67ebac
xscott2 <84044960+xscott2@users.noreply.github.com>
2023-06-07 19:31:01 -0700

    Add files via upload

---
+++ event.js
@@ -0,0 +1,15 @@
+//creates function pop up window on page load
+window.addEventListener("load", function(){
+    //set time out if desired
+    setTimeout(
+        function open(event){
+            //select document
+            document.querySelector(".popup").style.display = "block";
+        },
+        1000
+    )
+});
+//create on click functionality
+document.querySelector("#close").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none";
+});
